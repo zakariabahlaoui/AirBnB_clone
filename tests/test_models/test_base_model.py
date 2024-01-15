04b980ca29b60f9926385fd50b16732e363b31f7
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-01-15 02:56:52 +0100

    Add unit test for BaseModel __str__ method, ensuring correct string representation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,16 @@ class TestBaseModel(unittest.TestCase):
         m_model = BaseModel()
         ini_updated_at = m_model.updated_at
         curr_at = m_model.save()
-        self.assertNotEqual(ini_updated_at, curr_at)    
+        self.assertNotEqual(ini_updated_at, curr_at)  
+
+    def test_str(self):
+        m_model = BaseModel()
+        m_str = str(m_model)
+
+        self.assertIsInstance(m_str, str)
+        self.assertIn('BaseModel', m_str)
+        self.assertIn(m_model.id, m_str)
+        self.assertIn(str(m_model.__dict__), m_str)      
 
 
 if __name__ == '__main__':
